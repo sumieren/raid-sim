919db4e4673e87cbd526e47f7d65496882899d96
sumieren <aygoes@gmail.com>
2025-07-29 21:19:41 +0200

    Initial commit, planning structure

---
+++ main.py
@@ -0,0 +1,11 @@
+import hero, party, boss;
+
+def main():
+    # Select heroes and boss
+    # Create game (game.py?)
+    # Start game loop
+    # Display result
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ party.py
@@ -0,0 +1,7 @@
+class Party:
+    def __init__(self, size=4):
+        self.size = size
+        self.members = []
+
+    def add_member(self, hero):
+        pass
